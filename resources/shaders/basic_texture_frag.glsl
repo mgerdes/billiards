6ece68cae2169d14eed11c6dfc04f38d03d99e3d
Michael Gerdes <mgerdes11@gmail.com>
2016-03-26 09:41:02 -0400

    Added simple lighting equation to shader

--- resources/shaders/basic_texture_frag.glsl
+++ resources/shaders/basic_texture_frag.glsl
@@ -9,6 +9,11 @@ uniform sampler2D tex;
 out vec4 color;
 
 void main () {
+    vec3 light_position = vec3(1.0, 1.0, 1.0);
+
+    float kd = max(dot(light_position, frag_normal), 0.0);
+
     vec4 texel = texture(tex, frag_texture_coord);
     color = vec4(0.1, 0.1, 0.1, 1.0) + texel;
+    color *= kd;
 }
